8648d941ec00267e4f3b67f49c9e431825e5e182
fahrulrizaldi <fahrulrizaldi99@gmail.com>
2025-08-30 12:11:07 +0700

    second commit about if statemnt

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hellow world!');
+// first commit hello world!
+console.log('hello world!');
+
+//second commit if statement
+
+let angka = Math.random();
+console.log(angka);
+
+if (angka >= 0.5) {
+    console.log("angka lebih besar dari 0.5");
+}
+
+if (angka <= 0.5) {
+    console.log("angka lebih kecil dari 0.5");
+}
