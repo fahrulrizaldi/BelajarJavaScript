c26b02280b48893878fb8cbd8fa05440851da417
fahrulrizaldi <fahrulrizaldi99@gmail.com>
2025-09-01 13:14:11 +0700

    third commit

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ if (angka >= 0.5) {
 if (angka <= 0.5) {
     console.log("angka lebih kecil dari 0.5");
 }
+
+// third commit
+
+const iniHari = 'senin';
+
+if (iniHari === 'senin'){
+    console.log("Selamat Bekerja Kawan! ")
+}else if (iniHari === 'sabtu'){
+    console.log("Semoga Jangan Cepat Berlalu")
+}else if (iniHari === 'Rabu'){
+    console.log("Sudah Setengah Perjalanan kawan")
+}
