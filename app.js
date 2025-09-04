01b94b89968a3baec22f70eddb314615c890655a
fahrulrizaldi <fahrulrizaldi99@gmail.com>
2025-09-04 12:51:50 +0700

    fourth commit

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-// first commit hello world!
-console.log('hello world!');
+// // first commit hello world!
+// console.log('hello world!');
 
-//second commit if statement
+// //second commit if statement
 
-let angka = Math.random();
-console.log(angka);
+// let angka = Math.random();
+// console.log(angka);
 
-if (angka >= 0.5) {
-    console.log("angka lebih besar dari 0.5");
-}
+// if (angka >= 0.5) {
+//     console.log("angka lebih besar dari 0.5");
+// }
 
-if (angka <= 0.5) {
-    console.log("angka lebih kecil dari 0.5");
-}
+// if (angka <= 0.5) {
+//     console.log("angka lebih kecil dari 0.5");
+//}
 
 // third commit
 
-const iniHari = 'senin';
+// const iniHari = 'senin';
+
+// if (iniHari === 'senin'){
+//     console.log("Selamat Bekerja Kawan! ")
+// }else if (iniHari === 'sabtu'){
+//     console.log("Semoga Jangan Cepat Berlalu")
+// }else if (iniHari === 'Rabu'){
+//     console.log("Sudah Setengah Perjalanan kawan")
+// }
+
+// fourth commit 
+const iniHari = 'sabtu';
 
 if (iniHari === 'senin'){
-    console.log("Selamat Bekerja Kawan! ")
+    console.log("Selamat Bekerja Kawan !!!")
 }else if (iniHari === 'sabtu'){
-    console.log("Semoga Jangan Cepat Berlalu")
-}else if (iniHari === 'Rabu'){
-    console.log("Sudah Setengah Perjalanan kawan")
+    console.log("Selamat liburan kawan")
 }
